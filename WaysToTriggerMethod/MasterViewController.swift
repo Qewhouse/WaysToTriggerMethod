d8a10aa26026b2ca396d6a5f00fa2b56eaa9df47
Alexander Altman <qewhouse@gmail.com>
2023-04-30 02:46:10 +0300

    refactoring

--- WaysToTriggerMethod/MasterViewController.swift
+++ WaysToTriggerMethod/MasterViewController.swift
@@ -21,62 +21,54 @@ class MasterViewController: UIViewController {
     
     var completion: ((UIColor?) -> ())?
     
+    
+    
     private let buttonsStack: UIStackView = {
        let stack = UIStackView()
         stack.axis = .vertical
         stack.distribution = .equalSpacing
-        stack.spacing = 10
+        stack.spacing = 20
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
     
     lazy var backButton: UIButton = {
         let button = UIButton(type: .system)
-        button.layer.cornerRadius = 12
-        button.setTitleColor(.yellow, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
         button.setTitle("Blue Delegate", for: .normal)
-        button.backgroundColor = .darkGray
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .blue
         button.addTarget(self, action: #selector(goBackWithDelegate), for: .touchUpInside)
         return button
     }()
     
     lazy var backButton1: UIButton = {
         let button = UIButton(type: .system)
-        button.layer.cornerRadius = 12
-        button.setTitleColor(.yellow, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.black, for: .normal)
         button.setTitle("Green Notification", for: .normal)
-        button.backgroundColor = .darkGray
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .green
         button.addTarget(self, action: #selector(goBackWithNotification), for: .touchUpInside)
         return button
     }()
     
     lazy var backButton2: UIButton = {
         let button = UIButton(type: .system)
-        button.layer.cornerRadius = 12
-        button.setTitleColor(.yellow, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.red, for: .normal)
         button.setTitle("Yellow Responder Chain", for: .normal)
-        button.backgroundColor = .darkGray
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .yellow
         button.addTarget(nil, action: #selector(goBackWithResponder), for: .touchUpInside)
         return button
     }()
     
     lazy var backButton3: UIButton = {
         let button = UIButton(type: .system)
-        button.layer.cornerRadius = 12
-        button.setTitleColor(.yellow, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.black, for: .normal)
         button.setTitle("Red Closure", for: .normal)
-        button.backgroundColor = .darkGray
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
         button.addTarget(self, action: #selector(goBackWithClosure), for: .touchUpInside)
         return button
     }()
+    
+    lazy var buttonsArray = [backButton, backButton1, backButton2, backButton3]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -103,7 +95,6 @@ class MasterViewController: UIViewController {
         completion?(.red)
         dismiss(animated: true)
     }
-    
 }
 
 extension MasterViewController {
@@ -114,15 +105,28 @@ extension MasterViewController {
         buttonsStack.addArrangedSubview(backButton1)
         buttonsStack.addArrangedSubview(backButton2)
         buttonsStack.addArrangedSubview(backButton3)
+        
+        for button in buttonsArray {
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.black.cgColor
+            button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+            button.layer.cornerRadius = 12
+            button.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
 }
 
 extension MasterViewController {
     private func setConstraints() {
+        for button in buttonsArray {
+            button.heightAnchor.constraint(equalToConstant: view.frame.width * 1/5).isActive = true
+        }
+        
         NSLayoutConstraint.activate([
             buttonsStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             buttonsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            buttonsStack.widthAnchor.constraint(equalToConstant: view.frame.width * 2/3)
+            buttonsStack.widthAnchor.constraint(equalToConstant: view.frame.width * 2/3),
+
         ])
     }
 }
